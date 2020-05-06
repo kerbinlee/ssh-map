0eddf0bd0ba3d0bea4031afeac81dfbcde15a237
Kevin Lee <lee.kevin@yahoo.com>
2020-05-05 22:28:50 -0700

    Map improvements

--- map.js
+++ map.js
@@ -1,10 +1,28 @@
 var mymap = L.map('mapid').setView([15, 0], 2);
 
 var myStyle = {
-    "color": "#ff7800",
-    "weight": 1,
-    "opacity": 1
+    "fillColor": "BlanchedAlmond",
+    "color": "black",
+    "weight": .5,
+    "opacity": 1,
+    "fill": true,
+    "fillOpacity": 0.7
 };
+
+var popupData = function(ip) {
+    return function() {
+        var popupDataReq = new XMLHttpRequest();
+        popupDataReq.open("GET", "http://localhost:5000/" + ip, false);
+        popupDataReq.send();
+        var returnString = "";
+        var dateOptions = {
+            month: 'short', day: 'numeric', year: 'numeric',
+            timeZone: 'America/Los_Angeles' 
+        };
+        JSON.parse(popupDataReq.responseText).forEach(element => returnString += (new Date(element * 1000).toLocaleTimeString("en-US", dateOptions) + '<br/>'));
+        return returnString;
+    }
+}
 
 var mapLayerRequest = new XMLHttpRequest();
 mapLayerRequest.addEventListener("load", function() {L.geoJSON(JSON.parse(this.responseText), {style: myStyle}).addTo(mymap);});
@@ -12,10 +30,18 @@ mapLayerRequest.open("GET", "https://maythird.ddns.net/letsencrypt/countries.geo
 mapLayerRequest.send();
 
 function plotCord() {
-    var ipDataset = JSON.parse(this.responseText);
+    var ipDataset = JSON.parse(this.responseText);    
+    var myIcon = L.icon({
+        iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/PuTTY_icon_128px.png',
+        iconSize: [16, 16],
+        iconAnchor: [8, 8],
+        popupAnchor: [8, 8],
+        // shadowUrl: 'my-icon-shadow.png',
+        // shadowSize: [68, 95],
+        // shadowAnchor: [22, 94]
+    });
     for (ipData in ipDataset) {
-        console.log(ipData)
-        L.marker([ipDataset[ipData].latitude, ipDataset[ipData].longitude]).addTo(mymap);
+        L.marker([ipDataset[ipData].latitude, ipDataset[ipData].longitude], {icon: myIcon}).bindPopup(popupData(ipDataset[ipData].ip)).openPopup().addTo(mymap);
     }
 }
 
